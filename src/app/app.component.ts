4303e5b6c16edfd904eb74b0bdfde128718881bf
AleSpd <alessiospadafora.main@gmail.com>
2023-10-26 22:44:20 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EcommerceProj';
+
+  constructor(
+    private http: HttpClient
+  ) {
+    http.get('http://localhost:8080').subscribe((res) => {
+    })
+  }
 }
